49d590a8b186036e7e7eaa69e6a6d0b3de157608
CRISTIAN-SR <santillanrodriguezcristian@gmail.com>
2018-12-15 05:31:29 -0600

    diagonal difference

---
+++ 4-diagonaldifference.js
@@ -0,0 +1,117 @@
+/*
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix arr is shown below:
+
+1 2 3
+4 5 6
+9 8 9  
+The left-to-right diagonal = 1 +5 +9 = 15. The right to left diagonal = 3 + 5 + 9 = 17. Their absolute difference is | 15 - 17 | = 2.
+
+Function description
+
+Complete the diagonalDifference  function in the editor below. It must return an integer representing the absolute diagonal difference.
+
+diagonalDifference takes the following parameter:
+
+arr: an array of integers .
+Input Format
+
+The first line contains a single integer, n, the number of rows and columns in the matrix arr. 
+Each of the next n lines describes a row, arr[i] , and consists of n space-separated integers .
+arr[i][j]
+
+Constraints
+-100<= arr[i][j] <= 100
+Output Format
+
+Print the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+Sample Input
+
+3
+11 2 4
+4 5 6
+10 8 -12
+Sample Output
+
+15
+Explanation
+
+The primary diagonal is:
+
+11
+   5
+     -12
+Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+The secondary diagonal is:
+
+     4
+   5
+10
+Sum across the secondary diagonal: 4 + 5 + 10 = 19 
+Difference: |4 - 19| = 15
+
+Note: |x| is the absolute value of x
+*/
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the diagonalDifference function below.
+function diagonalDifference(arr) {
+    var primSum = 0;
+    var secSum = 0;
+    var j = arr.length - 1;
+    for (var i = 0; i < arr.length; i++) {
+        primSum = primSum + arr[i][i];
+    }
+    for (var i = 0; i < arr.length; i++) {
+        secSum = secSum + arr[i][j];
+        j--;
+    }
+    return (Math.abs(primSum - secSum));
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
