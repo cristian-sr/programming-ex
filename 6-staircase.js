d005f9a4fb2e7a4bc4b6bf7fa14c2c00c25213fc
Cristian - SR <santillanrodriguezcristian@gmail.com>
2018-12-21 07:50:34 -0600

    staircase

---
+++ 6-staircase.js
@@ -0,0 +1,133 @@
+/*
+Consider a staircase of size :
+
+   #
+  ##
+ ###
+####
+Observe that its base and height are both equal to , and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size n.
+
+Function Description
+
+Complete the staircase function in the editor below. It should print a staircase as described above.
+
+staircase has the following parameter(s):
+
+n: an integer
+Input Format
+
+A single integer, n, denoting the size of the staircase.
+
+Constraints
+
+ 0 < n <= 100.
+
+Output Format
+
+Print a staircase of size n using # symbols and spaces.
+
+Note: The last line must have 0 spaces in it.
+
+Sample Input
+
+6
+
+Sample Output
+Consider a staircase of size :
+
+   #
+  ##
+ ###
+####
+Observe that its base and height are both equal to , and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size .
+
+Function Description
+
+Complete the staircase function in the editor below. It should print a staircase as described above.
+
+staircase has the following parameter(s):
+
+n: an integer
+Input Format
+
+A single integer, , denoting the size of the staircase.
+
+Constraints
+
+ .
+
+Output Format
+
+Print a staircase of size  using # symbols and spaces.
+
+Note: The last line must have  spaces in it.
+
+Sample Input
+
+6 
+Sample Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+Explanation
+
+The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of .
+     #
+    ##
+   ###
+  ####
+ #####
+######
+Explanation
+
+The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of .
+*/
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the staircase function below.
+function staircase(n) {
+    var line = Array(n + 1).fill(' ');
+    line[n] = '\n';
+    for (var i = n - 1; i >= 0; i--) {
+        line[i] = '#';
+        process.stdout.write(line.join(''));
+    }
+
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    staircase(n);
+}
